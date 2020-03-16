9eaede885323a58897d16db5d82ce234644f1c8e
DOLIPRANE74 <t.noellardin@gmail.com>
2020-03-16 16:16:10 +0100

    Ajout de tests et de Commentaires

--- 2.2.1.v
+++ 2.2.1.v
@@ -3,19 +3,30 @@
 (*2.2.1 Exemple simple : l’identité polymorphe *)
 
 (*1*)
+(* -- Definition du tye identité -- *)
 Definition tid : Set := forall T: Set, T -> T.
+
+(* Définition de la fonction identitée *)
 Definition id : tid := fun T:Set => fun x:T => x.
 
 Compute id nat 3.
+Compute id nat 50.
+Compute id nat 10000. (* On peut voir qu'on se rapproche de la limite de la pile *)
 Compute id bool true.
 Compute id bool false.
 
 (*2*)
+(* Défintition de la fonction qui 1 lorsqu'elle à true 
+et 0 lorsqu'elle à false *)
 Definition nbtrue1 := fun b =>match b with
                       true => 1 | false => 0 end.
+
+(* Définition de la fonction qui applique la fonction nbtrue1 *)
 Definition id_bool : bool->nat := id (bool->nat) nbtrue1.
 Compute id_bool true.
 Compute id_bool false.
 
 (*3*)
-Compute id tid id.
+(* Application de la fonction id à elle même *)
+Compute id tid id. (* retourne x donc elle peux 
+                      s'appliquer à elle même *)
