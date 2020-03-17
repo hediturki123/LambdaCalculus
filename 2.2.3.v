537cc7005c840bd6da61fe703c72c4be35b4c732
hediturki123 <hedi.turki.sanekli@gmail.com>
2020-03-17 13:00:41 +0100

    echange done

--- 2.2.3.v
+++ 2.2.3.v
@@ -8,4 +8,10 @@ Definition pprod_bn : Set := forall T : Set, (bool->nat->T)->T.
 Definition pcpl_bn : bool->nat->pprod_bn := fun a b => fun T : Set => fun k : (bool->nat->T) =>k a b.
 Compute pcpl_bn true 3.
 
-Definition echange : pprod_nb->pprod_bn : fun k => k 
+Definition exchange_type:= fun a b => pcpl_bn b a.
+Compute exchange_type 3 true.
+Definition echange : pprod_nb->pprod_bn := fun k => k pprod_bn(exchange_type).
+Compute echange (pcpl_nb 3 true).
+Compute echange (pcpl_nb 6 false).
+
+
