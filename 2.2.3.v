4d1372ce663d29d52654253a2ea0e5ea86ee4900
hediturki123 <hedi.turki.sanekli@gmail.com>
2020-03-17 15:27:43 +0100

    il reste 2 questions

--- 2.2.3.v
+++ 2.2.3.v
@@ -8,10 +8,15 @@ Definition pprod_bn : Set := forall T : Set, (bool->nat->T)->T.
 Definition pcpl_bn : bool->nat->pprod_bn := fun a b => fun T : Set => fun k : (bool->nat->T) =>k a b.
 Compute pcpl_bn true 3.
 
-Definition exchange_type:= fun a b => pcpl_bn b a.
-Compute exchange_type 3 true.
-Definition echange : pprod_nb->pprod_bn := fun k => k pprod_bn(exchange_type).
+Definition permutation_nat_bool : nat->bool->pprod_bn := fun a b => pcpl_bn b a.
+Compute permutation_nat_bool 3 true.
+
+Definition echange : pprod_nb->pprod_bn := fun k => k pprod_bn(permutation_nat_bool).
 Compute echange (pcpl_nb 3 true).
 Compute echange (pcpl_nb 6 false).
 
 
+Definition pprod : Set -> Set -> Set := fun A B => forall T:Set, (A->B->T)->T.
+Definition pprod' (A B: Set) : Set := forall T:Set,(A->B->T)->T.
+Definition pcpl (A B : Set) := fun a b => fun T : Set =>fun k :(A->B->T) => k a b.
+Compute pcpl.
