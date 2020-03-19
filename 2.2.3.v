6a75386cd2f2a6bdcd86ca5de0c8992f5e5a78ba
DOLIPRANE74 <58179169+DOLIPRANE74@users.noreply.github.com>
2020-03-19 19:49:45 +0100

    Update 2.2.3.v

--- 2.2.3.v
+++ 2.2.3.v
@@ -52,6 +52,9 @@ Variable bool_vers_nat : bool->nat.
 Variable bool_vers_bool : bool->bool.
 
 (* Définition des fonctions inj1 *)
+(* Cette fonction va prendre un nat N et l'injecter dans la première fonction qui lui est donnée.
+La fonction va être appliqué à N et inj1 va rendre le résultat de cette fonction. *)
+
 Definition psom (A B: Set) : Set := forall T:Set,(A->T)->(B->T)->T.
 Definition inj1 (A B: Set) : A -> psom A B := fun u => fun T:Set => fun k => fun k'=>k u.
 Definition plus_un : nat->nat := fun n =>n+1.
@@ -60,6 +63,9 @@ Compute inj1 nat bool 2 nat plus_un bool_vers_nat.
 Compute inj1 bool nat true nat bool_vers_nat nat_Vers_nat.
 
 (* Définition des fonctions inj2 *)
+(* Pour cette fonction, elle va effectuer la même chose que inj1 sauf qu'au lieux de prendre la première fonction qui
+lui est donnée, elle va prendre la deuxième. *)
+
 Definition inj2 (A B: Set) : B -> psom A B := fun v => fun T:Set => fun k => fun k'=>k' v.
 Compute inj2 nat nat 2 nat nat_Vers_nat plus_un.
 Compute inj2 bool bool true bool bool_vers_bool bool_vers_bool.
