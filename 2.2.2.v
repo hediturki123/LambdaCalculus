f0e0f130638d97bec7c91c7f21c4eae859a15ed9
DOLIPRANE74 <t.noellardin@gmail.com>
2020-03-19 16:46:03 +0100

    Ajout de commentaire

--- 2.2.2.v
+++ 2.2.2.v
@@ -3,8 +3,8 @@
 (*1*)
 (* Définition des booléens *)
 Definition pbool : Set := forall T : Set, T -> T -> T.
-Definition ptr : pbool := fun T : Set => fun x : T => fun y : T => x.
-Definition pfa :  pbool := fun T : Set => fun x : T => fun y : T => y.
+Definition ptr : pbool := fun T : Set => fun x : T => fun y : T => x. (* TRUE *)
+Definition pfa :  pbool := fun T : Set => fun x : T => fun y : T => y. (* FALSE *)
 
 Compute ptr.
 Compute pfa.
@@ -12,7 +12,7 @@ Compute pfa.
 (*2*)
 (* Codage de la négation (Première version) *)
 Definition neg_bool_1 : pbool->pbool := fun b => fun T : Set => fun x y => b T y x.
-Compute neg_bool_1 ptr.
+Compute neg_bool_1 ptr. 
 Compute neg_bool_1 pfa.
 
 (* Codage de la négation (Deuxième version) *)
@@ -41,14 +41,13 @@ Definition cond : pbool->nat := fun b : pbool => b nat 3 5.
 Compute cond ptr.
 Compute cond pfa.
 Compute cond (neg_bool_1 ptr).
-Compute cond (neg_bool_1 (neg_bool_2 ptr)).
-Compute cond (conj ptr (neg_bool_1(neg_bool_2 ptr))).
-Compute cond (disj ptr (neg_bool_1(neg_bool_2 pfa))).
-Compute cond (disj pfa (neg_bool_1 ptr)).
+Compute cond (neg_bool_1 (neg_bool_2 ptr)). (* (!(!1)=1) => 3 *)
+Compute cond (conj ptr (neg_bool_1(neg_bool_2 ptr))). (* (1 & ((!(!1))=1)=>3) *)
+Compute cond (disj ptr (neg_bool_1(neg_bool_2 pfa))). (* (1 | ((!(!0))=0))=>3 *)
+Compute cond (disj pfa (neg_bool_1 ptr)). (* (0 | !1)=>5 *)
 
 (* 5 -- BONUS -- *)
 (* Définition de la fonction qui rend un booléen appliqué à lui même *)
 Definition id_bool: pbool->pbool := fun b => b pbool b b.
 Compute id_bool ptr.
 Compute id_bool pfa.
-
